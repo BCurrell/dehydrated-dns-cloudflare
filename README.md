6ff6a2d4c1438d0da36d41919b5607d507573c60
Brandon Currell <45363644+BCurrell@users.noreply.github.com>
2020-01-06 01:40:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# dehydrated-dns-cloudflare
+
+Dehydrated hook for DNS verification on Cloudflare in Python.
+
+Whilst there are already 2 recommended dehydrated hooks for DNS verification with Cloudflare, I still had reasons for creating this project.
+
+1) The one written in Python doesn't use the Cloudflare library, and requires you to install requirements globally.
+2) I wanted to get an understanding of dehydrated hooks before creating one for Hurricane Electric.
+
+You're welcome to use one of the existing (recommended) hooks, or you're welcome to use mine. Any feedback, bug reports, suggestions and/or pull requests are also welcome.
+
+## Requirements
+
+- dehydrated
+- python3
+- pipenv
+
+For the purposes of this "documentation", I'm assuming you already have these installed, or know how to install them.
+
+## Usage
+
+The following instructions assume the following:
+
+- You store your dehydrated hooks in `/etc/dehydrated/hooks/`
+
+You can store dehydrated hooks wherever you want, just update the paths when you run the commands.
+
+- You are using dehydrated with CLI args, not the config file
+
+If you prefer to use config files, read the dehydrated documentation and adjust accordingly.
+
+```shell
+cd /etc/dehydrated/hooks/
+git clone https://github.com/BCurrell/dehydrated-dns-cloudflare dns-cloudflare
+cd dns-cloudflare
+pipenv install
+cp .env.sample .env
+nano .env
+```
+
+```shell
+dehydrated -c -d testing.example.com -t dns-01 -k /etc/dehydrated/hooks/dns-cloudflare/hook.sh
+```
+
+## To-Do
+
+- [x] Initial commit with basic functionality.
+- [ ] Better logging. Or just some logging at all.
+- [ ] Better error handling.
---
+++ hook.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+from CloudFlare import CloudFlare
+from CloudFlare.exceptions import CloudFlareAPIError
+from dns.resolver import Resolver, NXDOMAIN
+from dns.exception import DNSException
+from dotenv import load_dotenv
+from sys import argv
+from time import sleep
+from tld import parse_tld
+
+load_dotenv()
+
+
+prefix = "_acme-challenge."
+cloudflare = CloudFlare()
+
+
+def get_zone_id(domain):
+    tld, domain, subdomain = parse_tld(domain, fix_protocol=True)
+
+    if domain is None:
+        # TODO: Fail with grace
+        exit(1)
+
+    fld = ".".join((domain, tld))
+
+    try:
+        zone = cloudflare.zones.get(params={'name': fld})[0]
+    except CloudFlareAPIError as e:
+        # TODO: Fail with grace
+        print(e)
+        exit(1)
+
+    return zone["id"], fld, subdomain
+
+
+def dns_lookup(name, resolver=None):
+    if resolver is None:
+        resolver = Resolver()
+
+    try:
+        yield from [str(record) for record in resolver.query(name, "TXT")]
+    except NXDOMAIN:
+        yield None
+    except DNSException as e:
+        # TODO: Fail with grace
+        print(e)
+        exit(1)
+
+
+def dns_verify(name, content):
+    retries = 3
+    resolver = Resolver()
+
+    if content is not None:
+        content = "\"{}\"".format(content)
+
+    for retry in range(retries):
+        sleep(10)
+
+        result = dns_lookup(name, resolver)
+
+        if content in result:
+            return True
+
+    return False
+
+
+def add_record(zone, name, content):
+    record = {
+        "name": name,
+        "content": content,
+        "type": "TXT",
+        "ttl": 120,
+        "proxied": False,
+    }
+
+    try:
+        result = cloudflare.zones.dns_records.post(zone, data=record)
+    except CloudFlareAPIError as e:
+        # TODO: Fail with grace
+        print(e)
+        exit(1)
+
+
+def remove_record(zone, name, content):
+    params = {
+        "name": name,
+        "content": content,
+    }
+
+    dns_records = cloudflare.zones.dns_records.get(zone, params=params)
+
+    try:
+        for record in dns_records:
+            result = cloudflare.zones.dns_records.delete(zone, record["id"])
+    except CloudFlareAPIError as e:
+        # TODO: Fail with grace
+        print(e)
+        exit(1)
+
+
+def deploy_challenge(*args):
+    name = prefix + args[0]
+    content = args[1]
+
+    zone, fld, subdomain = get_zone_id(name)
+
+    add_record(zone, name, content)
+
+    dns_verify(name, content)
+
+
+def clean_challenge(*args):
+    name = prefix + args[0]
+    content = args[1]
+
+    zone, fld, subdomain = get_zone_id(name)
+
+    remove_record(zone, name, content)
+
+    dns_verify(name, None)
+
+
+def main():
+    try:
+        operation, args = argv[1], argv[2:]
+    except IndexError:
+        # TODO: Fail with grace
+        print("Invalid argv!")
+        exit(1)
+
+    operations = {
+        "deploy_challenge": deploy_challenge,
+        "clean_challenge": clean_challenge,
+    }
+
+    if operation in operations:
+        operations[operation](*args)        
+
+
+if __name__ == "__main__":
+    main()
