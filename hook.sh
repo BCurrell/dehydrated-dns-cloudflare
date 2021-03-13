9e2a12deb36cce21013c875332fcf923b1a547de
Lee Watson <rev@revthefox.co.uk>
2021-03-13 22:20:25 +0000

    Fix the previous commit

--- hook.sh
+++ hook.sh
@@ -2,7 +2,6 @@
 
 HOOK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-HANDLER="$1"; shift
-if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge)$ ]]; then
+if [[ "${1}" =~ ^(deploy_challenge|clean_challenge)$ ]]; then
     poetry run python "${HOOK_DIR}/hook.py" "${@}"
 fi
