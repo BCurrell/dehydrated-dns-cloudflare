2b520111062b92e18f07303d4acb6822e61862cb
Lee Watson <rev@revthefox.co.uk>
2021-03-13 22:19:02 +0000

    Don't break when passing a hook function that there's no handler for

--- hook.sh
+++ hook.sh
@@ -2,4 +2,7 @@
 
 HOOK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-poetry run python "${HOOK_DIR}/hook.py" "${@}"
+HANDLER="$1"; shift
+if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge)$ ]]; then
+    poetry run python "${HOOK_DIR}/hook.py" "${@}"
+fi
